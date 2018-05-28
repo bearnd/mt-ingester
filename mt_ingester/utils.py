33babed68324890ee05c55d5c39bbe74b34de8e0
Adamos Kyriakou <somada141@gmail.com>
2018-05-29 07:24:12 +1000

    `utils.py`: Added logging decorator for the ingester methods.

--- mt_ingester/utils.py
+++ mt_ingester/utils.py
@@ -33,6 +33,31 @@ class EnumBase(enum.Enum):
                 return member
 
         return None
+
+
+def log_ingestion_of_document(
+    document_name: str
+):
+
+    # Define the actual decorator. This three-tier decorator functions are
+    # necessary when defining decorator functions with arguments.
+    def log_ingestion_of_document_decorator(func):
+        # Define the wrapper function.
+        def wrapper(self, *args, **kwargs):
+
+            msg = "Ingesting '{}' document"
+            msg_fmt = msg.format(document_name)
+            self.logger.debug(msg_fmt)
+
+            # Simply execute the decorated method with the provided arguments
+            # and return the result.
+            return func(self, *args, **kwargs)
+
+        return wrapper
+
+    return log_ingestion_of_document_decorator
+
+
 def return_first_item(func):
     """Decorator that can be used to return the first item of a callable's
     `list` return."""
